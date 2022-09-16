f7293b763a1ef051367be33f006815be405e0bfc
Kelvin M Matheka <beingmatheka@gmail.com>
2022-09-16 23:55:59 +0300

    Done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -15,16 +15,17 @@ function Home() {
     </div>
   );
 }
-
-{/* write an <About> component here */}
+const About = () => {
+  return <div id="about">About</div>
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
